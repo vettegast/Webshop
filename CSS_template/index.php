dfb4387b0cb5037250e1fa5d9d35ea67f9e2e887
marterido <malblas6491@student.landstede.nl>
2020-03-23 10:45:04 +0100

    added all files for the header

---
+++ CSS_template/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="stylesheet" href="CSS_template.css">
+        <title>Test</title>
+    </head>
+    <body>
+        <p class="title">Amazing title</p>
+        <div class="white_box box_text">
+            <form method="post">
+                <label for="input">Product Name:</label><br>
+                <input type="text" class="input_text" id="input">
+                <br>
+                <label for="categorie">Categorie:</label><br>
+                <select id="categorie">
+                    <option value="coffee">Coffee</option>
+                    <option value="tea">Tea</option>
+                    <option value="milk">Milk</option>
+                </select>
+                <br>
+                <label for="number">Amount:</label><br>
+                <input type="number" min="1" max="100" id="number" class="input_number">
+                <br>
+                <input type="submit" value="Submit" class="input_button">
+            </form>
+            <hr>
+            <p class="heading">Amazing heading</p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda debitis dicta dolore? Debitis dignissimos,
+                dolor dolorum earum iste laborum magni minima molestias nemo optio quibusdam rerum saepe ut, veritatis!
+                Deleniti enim illum itaque pariatur recusandae! A, accusantium cum ea eos eveniet explicabo id illum iste iusto magni nam nemo
+                omnis quam quidem quis ratione reprehenderit repudiandae sequi sint tempora.
+            </p>
+        </div>
+    </body>
+</html>
---
+++ index.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    var element = document.getElementById("topnav");
+    element.classList.toggle("unset");
+}
