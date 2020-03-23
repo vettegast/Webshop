ecb85272c21ee7a74a5cbeda64c983795361e65c
marterido <malblas6491@student.landstede.nl>
2020-03-23 10:53:53 +0100

    moved home.css to css folder

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Home</title>
 
-        <link rel="stylesheet" href="home.css">
+        <link rel="stylesheet" href="CSS/home.css">
         <link rel="stylesheet" href="CSS_template/CSS_template.css">
         <script src="index.js"></script>
     </head>
