d32a53967468df7d13f4f3fe6d7b35cba079d81c
Jochem <jochemvdheiden@gmail.com>
2020-04-11 20:49:29 +0200

    show error message if entered email already exists in database

--- signup.php
+++ signup.php
@@ -4,41 +4,52 @@ include 'classes/login.php';
 session_start();
 
 
-if (isset($_POST['submit']))
-{
+if (isset($_POST['submit'])) {
     signup();
 }
 
 function signup()
 {
 
-    // execute login function
+    // execute login function and get all users from database
 
     $users = new login();
+    $Database = $users->index();
 
 
-    // store email, password and the hashed password in variables
+    if (!empty($_POST['email-input']) && !empty($_POST['password-input'])) {
 
-    $email = $_POST['email-input'];
-    $password = $_POST['password-input'];
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    // create a new users in the database with the variables
+        if (!empty($Database[0])) {
+            echo "This email address is already in use";
+        } else {
 
-    $Database = $users->create($email, $hashed_password, 0);
+            // store email, password and the hashed password in variables
 
-    //empty the email, password and database
+            $email = $_POST['email-input'];
+            $password = $_POST['password-input'];
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $Database = array();
+            // create a new users in the database with the variables
 
-    $email = "";
-    $password = "";
+            $Database = $users->create($email, $hashed_password, 0);
+
+            //empty the email, password and database
+
+            $Database = array();
+
+            $email = "";
+            $password = "";
+        }
+    }
+    else {
+        echo "Please enter your email address and password";
+    }
+
 
 }
 
 ?>
-
-
 
 
 <html>
@@ -46,8 +57,10 @@ function signup()
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/login.css">
     <title>Login</title>
 </head>
 
